fa041f71c4ffe0e04490787caa37f0d1aede0e52
saf112 <35746116+saf112@users.noreply.github.com>
2018-04-06 14:17:05 -0400

    Add files via upload

--- pipeline.m
+++ pipeline.m
@@ -50,47 +50,76 @@ for i=1:1:height(pat_table)
 end
 
 features(:,k:end)=[];
+features=features';
+labels=pat_table.stage_sumajc;
 DNEcount
 nan_count
 
-cd ../../Machine Learning/Project ;
-save('features.mat','features')
+cd ../MachineLearning/Project ;
+save('features.mat','features');
+save('labels.mat','labels');
+save('pat_table.mat','pat_table');
 
-%% split data to train / CV / testing 
-features=features';
-temp=[];
-cv=[];
-test=[];
+%% SPLIT FEATURES TO TEST/TRAIN FEATURES
+%goal: 20% test data, 80% training data
+%features: patients x features
+%labels: patients x 1
+load features.mat
+load labels.mat
+
+test_ind=[];
 for i=1:1:3
     s=find(labels==i);
     n=length(s);
-    hold=randsample(s,floor(n*0.2));
-%     l=length(hold);
-%     h1=hold(1:l/2);
-%     h2=hold(l/2 +1:end);
-%     
-%     cv=[cv;h1];
-    h2=hold;
-    test=[test;h2];
+    ind=randsample(s,floor(n*0.2));
+    test_ind=[test_ind;ind];
 end
-% hold=[cv;test];
-% cv_labels=labels(cv,1);
-% cv=features(cv,:);
-hold=test;
-test_labels=labels(test,1);
-test=features(test,:);
-features(hold,:)=[];
-labels(hold,:)=[];
+% get test data & labels
+test_labels=labels(test_ind,1);
+test_data=features(test_ind,:);
+% get train data & labels
+train_data=features;
+train_labels=labels;
+train_data(test_ind,:)=[];
+train_labels(test_ind,:)=[];
 
-%% train random forest
-model=fitctree(features,labels)
-pred=predict(features)
+%% TRAIN DECISION TREE(s) 
+%goal: using train data & labels, train and optimatize decision tree(s)
 
+%cross val default: 10 fold cross validation
+model=fitctree(train_data,train_labels,'CrossVal','on')
 
-%% get cell nbhd features for random patient
+%to view k fold decision trees
+% for i=1:1:length(model.Trained)
+%     view(model.Trained{i},'Mode','graph')
+% end
+
+%training accuracy 
+train_acc=zeros(length(model.Trained),1);
+for i=1:1:length(model.Trained)
+    p=predict(model.Trained{i},train_data);
+    acc=length(find(p==train_labels))/length(train_labels);
+    train_acc(i,1)=acc;
+end
+disp('avg train acc')
+avg_train_acc=mean(train_acc)
+
+%testing accuracy 
+test_acc=zeros(length(model.Trained),1);
+for i=1:1:length(model.Trained)
+    p=predict(model.Trained{i},test_data);
+    acc=length(find(p==test_labels))/length(test_labels);
+    test_acc(i,1)=acc;
+end
+disp('avg train acc')
+avg_test_acc=mean(test_acc)
+
+%% GET NEIGHBORHOOD CELL FEATURES FOR A RANDOM PATIENT
+load pat_table.mat
+
 cd ../../LAB ;
 filepath='TMA_Core_Files';
-load pat.mat;
+
 
 num_pat=height(pat_table);
 pat_num=randi([1 num_pat],1);
@@ -99,47 +128,62 @@ fname=fullfile(TME_filepath,strcat(name,'.mat'));
 load(fname);
 num_cells=length(bm_data(:,1));
 
-h=150;
-num_features=24976;
+h=100;
+num_features=1540;
 cell_features=zeros(num_cells,num_features);
+nan_count=0;
+delcells=[];
+k=1;
 for j=1:1:num_cells
-    j
     cellx=x(1,j);
     celly=y(1,j);
     distx=abs(x'-cellx);
     disty=abs(y'-celly);
     ind=find(distx<=h & disty<=h);
-    
-    %disp('Cells found in nbhd: ')
-    %length(ind)
-    
-    cells=bm_data(ind,:);
+
     bmarkers=cellstr(bm_names);
     cellInds=1:4:length(bmarkers);
-    subInds=1:length(bmarkers);
-    subInds(cellInds)=[];
     
-    cells_corr=squareform(1-pdist(cells','cosine'));
-%     cells_epithelial=epithelial(1,ind);
-%     epi=find(cells_epithelial==1);
-%     strom=find(cells_epithelial==2);
-%     epi_data=cells(epi,subInds);
-%     strom_data=cells(strom,cellInds);
-%     
-%     epi_corr=squareform(1-pdist(epi_data','cosine'));
-%     strom_corr=squareform(1-pdist(strom_data','cosine'));
-%    
-%     [meanMat, stdMat]=meanstd_corrmat(epi_corr,strom_corr, epi_data, strom_data);
-%    
-    N=length(cells_corr(:,1));
-    f_vec=zeros((N*(N-1))/2,1);
-    c=1;
-    for i=1:1:N
-        for m=i+1:1:N
-            f_vec(c,1)=cells_corr(i,m);
-            c=c+1;
-        end
+    cells=bm_data(ind,cellInds);
+    
+    cells_corr=1-pdist(cells','cosine');
+    
+    if sum(isnan(cells_corr))
+        nan_count=nan_count+1;
+        delcells=[delcells;j];
+        continue
     end
-
-    cell_features(j,:)=f_vec';
+    
+    cell_features(k,:)=cells_corr;
+    k=k+1;
 end
+cell_features(k:end,:)=[];
+x(delcells)=[];
+y(delcells)=[];
+labels=pat_table.stage_sumajc;
+stage=labels(pat_num);
+nan_count
+
+cd ../MachineLearning/Project
+save('patient_name.mat','name');
+save('cell_features.mat','cell_features');
+save('pat_stage.mat','stage');
+save('pat_x.mat','x');
+save('pat_y.mat','y');
+
+%% PREDICT STAGE BASED ON CELL FEATURES AND PLOT ACCORDING TO LOCATION 
+
+load cell_features.mat
+load pat_stage.mat
+load pat_x.mat
+load pat_y.mat
+
+pred=zeros(length(cell_features(:,1)),length(model.Trained));
+for i=1:1:length(model.Trained)
+    p=predict(model.Trained{i},cell_features);
+    pred(:,i)=p;
+end
+predictions=round(mean(pred,2));
+c=[1 0 0;0 1 0;0 0 1];
+stage
+scatter(x',y',[],c(predictions,:))
